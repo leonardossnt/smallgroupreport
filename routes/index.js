1268a70aa68c41e8b3fd08694f2c2055d29ecb86
Leonardo Santana <leonardossnt@gmail.com>
2017-06-02 14:30:38 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Relatório de Grupo de Amigos' });
+});
+
+module.exports = router;
